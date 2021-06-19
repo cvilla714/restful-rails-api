22261f170e1b725e90bc48ee843a8e2dcb498ab8
Cosmel Villalobos <cosmelvg@gmail.com>
2021-06-19 19:49:37 -0400

    creted the public controller to grant access to public method

--- app/controllers/concerns/secure.rb
+++ app/controllers/concerns/secure.rb
@@ -36,3 +36,13 @@ def authenticate_request!
 rescue JWT::VerificationError, JWT::DecodeError
   render json: { errors: ['Not Authenticated'] }, status: :unauthorized
 end
+
+def scope_included
+  if SCOPES[request.env['PATH_INFO']].nil?
+    true
+  else
+    # The intersection of the scopes included in the given JWT and the ones in the SCOPES hash needed to access
+    # the PATH_INFO, should contain at least one element
+    (String(@auth_payload['scope']).split(' ') & (SCOPES[request.env['PATH_INFO']])).any?
+  end
+end
