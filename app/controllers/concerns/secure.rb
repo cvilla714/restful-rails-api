2532e3932b069de62f284cf47dbae367d3defbc2
Cosmel Villalobos <cosmelvg@gmail.com>
2021-06-19 19:39:01 -0400

    if the token is present then it will passed to jsonwebtoken verify

--- app/controllers/concerns/secure.rb
+++ app/controllers/concerns/secure.rb
@@ -1,20 +1,23 @@
 module Secured
-    extend ActiveSupport::Concern
-  
-    included do
-      before_action :authenticate_request!
-    end
+  extend ActiveSupport::Concern
 
-    private
+  included do
+    before_action :authenticate_request!
+  end
 
-    def authenticate_request!
-      auth_token
-    rescue JWT::VerificationError, JWT::DecodeError
-      render json: { errors: ['Not Authenticated'] }, status: :unauthorized
-    end
+  private
 
-    def http_token
-        if request.headers['Authorization'].present?
-          request.headers['Authorization'].split(' ').last
-        end
-      end
+  def authenticate_request!
+    auth_token
+  rescue JWT::VerificationError, JWT::DecodeError
+    render json: { errors: ['Not Authenticated'] }, status: :unauthorized
+  end
+
+  def http_token
+    request.headers['Authorization'].split(' ').last if request.headers['Authorization'].present?
+  end
+
+  def auth_token
+    JsonWebToken.verify(http_token)
+  end
+end
