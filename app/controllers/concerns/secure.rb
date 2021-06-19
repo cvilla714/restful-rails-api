34d577216aa3a412ae12eb7eb828ac86bd9c47b6
Cosmel Villalobos <cosmelvg@gmail.com>
2021-06-19 19:37:03 -0400

    added method to render an error if the token is not correct

--- app/controllers/concerns/secure.rb
+++ app/controllers/concerns/secure.rb
@@ -4,3 +4,11 @@ module Secured
     included do
       before_action :authenticate_request!
     end
+
+    private
+
+    def authenticate_request!
+      auth_token
+    rescue JWT::VerificationError, JWT::DecodeError
+      render json: { errors: ['Not Authenticated'] }, status: :unauthorized
+    end
