260fe49dedd26e8588a45a126b8d76b798780e62
Cosmel Villalobos <cosmelvg@gmail.com>
2021-06-19 19:29:53 -0400

    created the class JsonWebToken to decript the token coming from the front end

---
+++ lib/json_web_token.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require 'net/http'
+require 'uri'
+
+class JsonWebToken
+  def self.verify(token)
+    JWT.decode(token, nil,
+               true, # Verify the signature of this token
+               algorithms: 'RS256',
+               iss: 'https://dev-v88tfgqc.us.auth0.com/',
+               verify_iss: true,
+               aud: Rails.application.secrets.auth0_api_audience,
+               verify_aud: true) do |header|
+      jwks_hash[header['kid']]
+    end
+  end
+end
