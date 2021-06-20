51d28d0f0d3f0b08707b9ef7b368ec7675688f39
Cosmel Villalobos <cosmelvg@gmail.com>
2021-06-19 22:00:26 -0400

    added the root route and private route to display the correct data from each

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get '/private', to: 'private#private'
+  # get '/', to: 'public_controller#public'
+  root to: 'public#public'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
