c6dba3886d38308c020ee96c21f33845248550df
Cosmel Villalobos <cosmelvg@gmail.com>
2021-06-19 21:59:17 -0400

    removed the concern Secured

--- app/controllers/private_controller.rb
+++ app/controllers/private_controller.rb
@@ -1,10 +1,8 @@
 # frozen_string_literal: true
 
 class PrivateController < ActionController::API
-  include Secured
-
   def private
-    render json: 'Hello from a private endpoint! You need to be authenticated to see this.'
+    render json: 'Hello from a private endpoint! You need to be authenticated to see this'
   end
 
   def private_scoped
