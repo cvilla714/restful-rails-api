babe53d550bb0be13aa6ae03832ca30983c32503
Cosmel Villalobos <cosmelvg@gmail.com>
2021-06-20 22:15:28 -0400

    added routes for showinfo and print in the console the token and added sendinfo to display the user after token decoded

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   get '/private', to: 'private#private'
   # get '/', to: 'public_controller#public'
   root to: 'public#public'
+
+  get '/showinfo', to: 'display#index'
+
+  get '/sendinfo', to: 'token#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
