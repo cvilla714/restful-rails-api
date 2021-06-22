85d476f65fa57d2d8a2761dc0bbd12975e52670f
Cosmel Villalobos <cosmelvg@gmail.com>
2021-06-22 18:25:56 -0400

    use faraday to make the request to the front end of the user credetials once token is valid

--- app/controllers/private_controller.rb
+++ app/controllers/private_controller.rb
@@ -1,23 +1,17 @@
 # frozen_string_literal: true
 
 class PrivateController < ActionController::API
-  def private
-    require 'net/http'
-    require 'json'
+  def index
+    require 'faraday'
 
-    access_token = request.headers['Authorization'].split(' ')
+    url = 'https://dev-v88tfgqc.us.auth0.com/userinfo'
+
+    access_token = request.headers['authorization'].split(' ').last
     puts access_token
-    @url = 'https://dev-v88tfgqc.us.auth0.com/userinfo'
-    request['Authorization'] =
-      `Bearer #{access_token}`
-    @uri = URI(@url)
-    @response = Net::HTTP.get(@uri)
-    @output = JSON.parse(@response)
-
-    # render json: 'Hello from a private endpoint! You need to be authenticated to see this'
-  end
-
-  def private_scoped
-    render json: { message: 'Hello from a private endpoint! You need to be authenticated and have a scope of read:messages to see this.' }
+    response = Faraday.get(url, { 'authorization' => 'Bearer' + access_token })
+    p response.status
+    p response.body
+    response = Faraday.get(url,
+                           { 'authorization' => 'Bearer' + access_token })
   end
 end
