5b0fa44eeb4004cf763b150a58b071cc5010d09b
Cosmel Villalobos <cosmelvg@gmail.com>
2021-06-22 01:20:11 -0400

    added new routes to get token and create as well

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
   get '/private', to: 'private#private'
+  post '/private', to: 'privatec#create'
   get '/private-scope', to: 'private#private_scoped'
   # get '/', to: 'public_controller#public'
   root to: 'public#public'
 
   get '/showinfo', to: 'display#index'
+  get '/callapi', to: 'display#callapi'
 
   get '/sendinfo', to: 'token#index'
+  get '/gettoken', to: 'token#test_api'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
