2c5dd8e2d8517a50bb368899ebd9c9fdf829460b
Cosmel Villalobos <cosmelvg@gmail.com>
2021-06-27 04:17:28 -0400

    new method to get the access token

--- app/controllers/token_controller.rb
+++ app/controllers/token_controller.rb
@@ -1,22 +1,41 @@
 class TokenController < ActionController::API
-  def index
+  def gettoken
     require 'uri'
     require 'net/http'
+    require 'openssl'
 
-    url = URI('https://dev-v88tfgqc.us.auth0.com/userinfo')
-    # url = URI('https://localhost:5000/private')
+    url = URI('https://dev-v88tfgqc.us.auth0.com/oauth/token')
 
     http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
-    request = Net::HTTP::Get.new(url)
-    request['Authorization'] =
-      'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IjE3eUdnNWdiVEZkSkd1U25pNDdXZyJ9.eyJpc3MiOiJodHRwczovL2Rldi12ODh0ZmdxYy51cy5hdXRoMC5jb20vIiwic3ViIjoidjFVMFdXOXFWa0E0MGFyTTM1RlZhZ2tVZ3dKVUhGcmlAY2xpZW50cyIsImF1ZCI6InRoaXMgaXMgdGhlIGJhY2sgZW5kIGFwaSBjb25uZWN0aW9uIiwiaWF0IjoxNjI0MjIyNDcyLCJleHAiOjE2MjQzMDg4NzIsImF6cCI6InYxVTBXVzlxVmtBNDBhck0zNUZWYWdrVWd3SlVIRnJpIiwiZ3R5IjoiY2xpZW50LWNyZWRlbnRpYWxzIn0.Io6F1ClCFWnwt6Ju6aNpfnsSXdTes3_riWQJua5nQ1XSeJ2h3GcIlsQQnd4CWZwofJz2dqvIPZrh6twXUQHtn_eoAS3HNQlu4YyNOhl5IruA2VaO0ydcfv04av6jB-HM9isKfSs-dmjAYbG0DK1cpH3Qn-_sh8pVS2OODvVHr9FO3jxxBoDdLYbAhFGD9QsJORM4nh-ilQwC9PYwuDWA4uCn1be-dAzV2eex-gOWpzY3IA-oQk_YWi6EXizu2Wab2jvkdafk81tVz-vKipMhuWmDuYFKT8inFa7EY2oZvF1JnpDgSRB46ee9IctYy0U2_Yw9NGoTEC-9oRA0X3iqcQ'
+    request = Net::HTTP::Post.new(url)
+    # request["content-type"] = 'application/x-www-form-urlencoded'
+    request['content-type'] = 'application/json'
+    request.body = '{"client_id":"v1U0WW9qVkA40arM35FVagkUgwJUHFri","client_secret":"yIeEl_jPoMwd9RMcBLpkEWwPLrx7EA4ml0Oal2ezrHCn3Bzno4mWKqS6OHVHvGc2","audience":"this is the back end api connection","grant_type":"client_credentials"}'
 
     response = http.request(request)
     puts response.read_body
   end
 
-  def test_api
+  def callapi
+    require 'uri'
+    require 'net/http'
+
+    url = URI('http://localhost:5000/private')
+
+    http = Net::HTTP.new(url.host, url.port)
+
+    request = Net::HTTP::Get.new(url)
+    request['authorization'] =
+      'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IjE3eUdnNWdiVEZkSkd1U25pNDdXZyJ9.eyJpc3MiOiJodHRwczovL2Rldi12ODh0ZmdxYy51cy5hdXRoMC5jb20vIiwic3ViIjoidjFVMFdXOXFWa0E0MGFyTTM1RlZhZ2tVZ3dKVUhGcmlAY2xpZW50cyIsImF1ZCI6InRoaXMgaXMgdGhlIGJhY2sgZW5kIGFwaSBjb25uZWN0aW9uIiwiaWF0IjoxNjI0NzgxMzI1LCJleHAiOjE2MjQ4Njc3MjUsImF6cCI6InYxVTBXVzlxVmtBNDBhck0zNUZWYWdrVWd3SlVIRnJpIiwic2NvcGUiOiJjcmVhdGU6dXNlcnMgcmVhZDp1c2VycyIsImd0eSI6ImNsaWVudC1jcmVkZW50aWFscyJ9.rgMtDST0hINWp7VTkX-ualAvRPrq2OoD7Pq6cnnjIduR2njjJ_k5JDVyQDAsKfzu5U4IoUPjY7TuhrV1B8n8bAdit2n262ERu9MoAjs6LgRLR16MxGVyPek-vKLy4FKx3nKoP-K2JnzKv85ea2rAF3UthXEPpkcq_oCWc_TurrwARlYQZO6Q-jSk6PanofHdUugXif39uO_c5rBBo0YoC5VptKbYuyvcuFE1zyRylNMfwVehBS-t3bco41aHnroi0b3qy2qfwfN3v--be4WIuLFEsVsWVJ4c4LUPioFBkjVRfuACVuIfLDqbxm3Ey36oPPzDQOLyqBvPs8jMGMb4wA'
+
+    response = http.request(request)
+    puts response.read_body
+  end
+
+  def testyourapi
     require 'uri'
     require 'net/http'
 
